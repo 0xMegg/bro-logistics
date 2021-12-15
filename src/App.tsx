303d428d97e24eeb5c198ac1ee86fc2bfce823f6
finero-d <finero91@gmail.com>
2021-12-16 03:44:48 +0900

    npx create-react-app . --template=typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-// import Header from './components/mobile/Header';
-// import BlockOne from './components/mobile/promotions/BlockOne';
-// import BlockTwo from './components/mobile/promotions/BlockTwo';
-// import BlockThree from './components/mobile/promotions/BlockThree';
-// import BlockFour from './components/mobile/promotions/BlockFour';
-// import ItemList from './components/mobile/ItemList';
-// import Footer from './components/mobile/Footer';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header></Header>
-      <BlockOne></BlockOne>
-      <BlockTwo></BlockTwo>
-      <BlockThree></BlockThree>
-      <BlockFour></BlockFour>
-      <ItemList></ItemList>
-      <Footer></Footer> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
