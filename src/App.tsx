971a6afd168dd089c8f5e10075b504334d869f04
finero-d <finero91@gmail.com>
2021-12-16 03:40:18 +0900

    before ts change commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+// import Header from './components/mobile/Header';
+// import BlockOne from './components/mobile/promotions/BlockOne';
+// import BlockTwo from './components/mobile/promotions/BlockTwo';
+// import BlockThree from './components/mobile/promotions/BlockThree';
+// import BlockFour from './components/mobile/promotions/BlockFour';
+// import ItemList from './components/mobile/ItemList';
+// import Footer from './components/mobile/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header></Header>
+      <BlockOne></BlockOne>
+      <BlockTwo></BlockTwo>
+      <BlockThree></BlockThree>
+      <BlockFour></BlockFour>
+      <ItemList></ItemList>
+      <Footer></Footer> */}
+    </div>
+  );
+}
+
+export default App;
